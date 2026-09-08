d1827d17c1d86c17505fef3c3c81a1f16eb26d62
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-08 17:31:22 +0200

    serializer abstraction update

--- src/DandyEventStore/Serialization/IEventStoreSerializer.cs
+++ src/DandyEventStore/Serialization/IEventStoreSerializer.cs
@@ -2,6 +2,6 @@ namespace DandyEventStore.Serialization;
 
 public interface IEventStoreSerializer
 {
-    string Serialize(object eventItem);
-    object Deserialize(string eventItem, Type eventType);
+    string Serialize(object eventItem, Type runtimeType);
+    object? Deserialize(string eventItem, Type eventType);
 }
