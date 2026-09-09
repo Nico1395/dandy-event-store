0a431fc2b58eb5ee5cfde574c8fc00e730511192
nico1395 <119791859+Nico1395@users.noreply.github.com>
2026-09-09 10:12:07 +0200

    reordered properties of raw envelopes

--- src/DandyEventStore/Persistence/RawEnvelope.cs
+++ src/DandyEventStore/Persistence/RawEnvelope.cs
@@ -4,7 +4,7 @@ public sealed class RawEnvelope
 {
     public required string StreamId { get; init; }
     public required string Payload { get; init; }
-    public required string EventKey { get; init; }
     public required long Version { get; init; }
     public required DateTime Timestamp { get; init; }
+    public required string EventKey { get; init; }
 }
